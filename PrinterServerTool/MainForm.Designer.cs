52b8bc88eca891e13d5115110dbb2117c7871fec
MD KAMRUL HASAN <114093612+chasmkhasan@users.noreply.github.com>
2023-12-09 22:19:04 +0100

    Windows Form updated.

--- PrinterServerTool/MainForm.Designer.cs
+++ PrinterServerTool/MainForm.Designer.cs
@@ -30,14 +30,16 @@
 		{
 			label1 = new Label();
 			drpPrinterList = new ComboBox();
-			btnInstallServer = new Button();
+			btnInstallPrinter = new Button();
+			btnSearch = new Button();
+			lstOfPrinterName = new ListBox();
 			SuspendLayout();
 			// 
 			// label1
 			// 
 			label1.AutoSize = true;
 			label1.ForeColor = Color.Red;
-			label1.Location = new Point(78, 39);
+			label1.Location = new Point(119, 38);
 			label1.Name = "label1";
 			label1.Size = new Size(77, 15);
 			label1.TabIndex = 0;
@@ -48,24 +50,45 @@
 			drpPrinterList.FormattingEnabled = true;
 			drpPrinterList.Location = new Point(30, 76);
 			drpPrinterList.Name = "drpPrinterList";
-			drpPrinterList.Size = new Size(175, 23);
+			drpPrinterList.Size = new Size(261, 23);
 			drpPrinterList.TabIndex = 1;
 			// 
-			// btnInstallServer
+			// btnInstallPrinter
 			// 
-			btnInstallServer.Location = new Point(243, 78);
-			btnInstallServer.Name = "btnInstallServer";
-			btnInstallServer.Size = new Size(109, 23);
-			btnInstallServer.TabIndex = 2;
-			btnInstallServer.Text = "Install Server";
-			btnInstallServer.UseVisualStyleBackColor = true;
+			btnInstallPrinter.ForeColor = Color.FromArgb(0, 192, 0);
+			btnInstallPrinter.Location = new Point(119, 294);
+			btnInstallPrinter.Name = "btnInstallPrinter";
+			btnInstallPrinter.Size = new Size(109, 23);
+			btnInstallPrinter.TabIndex = 2;
+			btnInstallPrinter.Text = "Install Server";
+			btnInstallPrinter.UseVisualStyleBackColor = true;
+			// 
+			// btnSearch
+			// 
+			btnSearch.Location = new Point(321, 75);
+			btnSearch.Name = "btnSearch";
+			btnSearch.Size = new Size(109, 23);
+			btnSearch.TabIndex = 3;
+			btnSearch.Text = "Search";
+			btnSearch.UseVisualStyleBackColor = true;
+			// 
+			// lstOfPrinterName
+			// 
+			lstOfPrinterName.FormattingEnabled = true;
+			lstOfPrinterName.ItemHeight = 15;
+			lstOfPrinterName.Location = new Point(33, 123);
+			lstOfPrinterName.Name = "lstOfPrinterName";
+			lstOfPrinterName.Size = new Size(258, 154);
+			lstOfPrinterName.TabIndex = 4;
 			// 
 			// MainForm
 			// 
 			AutoScaleDimensions = new SizeF(7F, 15F);
 			AutoScaleMode = AutoScaleMode.Font;
-			ClientSize = new Size(377, 173);
-			Controls.Add(btnInstallServer);
+			ClientSize = new Size(458, 347);
+			Controls.Add(lstOfPrinterName);
+			Controls.Add(btnSearch);
+			Controls.Add(btnInstallPrinter);
 			Controls.Add(drpPrinterList);
 			Controls.Add(label1);
 			Name = "MainForm";
@@ -78,6 +101,8 @@
 
 		private Label label1;
 		private ComboBox drpPrinterList;
-		private Button btnInstallServer;
+		private Button btnInstallPrinter;
+		private Button btnSearch;
+		private ListBox lstOfPrinterName;
 	}
 }
