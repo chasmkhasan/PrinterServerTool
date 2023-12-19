a967d399416901c810fefb32cb812eb059173b4a
MD KAMRUL HASAN <114093612+chasmkhasan@users.noreply.github.com>
2023-12-19 15:01:56 +0100

    ProgressBar Implemented

--- PrinterServerTool/MainForm.cs
+++ PrinterServerTool/MainForm.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Drawing.Printing;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace PrinterServerTool
 {
@@ -84,25 +85,72 @@ namespace PrinterServerTool
 
 		private async void btnSearch_Click(object sender, EventArgs e)
 		{
-			bool ok = ReadChoice();
+			// Display a message to indicate that the search is in progress
+			//MessageBox.Show("Searching for shared printers. Please wait...");
+
+			//Set the initial progress bar value
+			prgBar.Value = 0;
+
+			bool ok = await Task.Run(() => SearchForPrinters());
+
+			//bool ok = ReadChoice();
 
 			if (ok)
 			{
-				List<string> sharedPrinters = await Task.Run(() => printerMgt.GetSharedPrintersAsync());
-
-				MessageBox.Show("Searching for shared printers. Please wait...");
-
-				lstOfPrinterName.Items.Clear();
-
-				foreach (string printerName in sharedPrinters)
-				{
-					lstOfPrinterName.Items.Add($"Server: {printerName}");
-				}
+				MessageBox.Show("Search Completed successfully.", "Search Result");
 			}
 			else
 			{
 				// Inform the user that no shared printers were found
 				MessageBox.Show("No shared printers found.", "Search Results");
+			}
+		}
+
+		private async Task<bool> SearchForPrinters()
+		{
+			bool result = false;
+
+			List<string> sharedPrinters = await Task.Run(() => printerMgt.GetSharedPrintersAsync());
+
+			// Update progress bar when the search is complete
+			UpdateProgressBar(100);
+
+			// Handle the results
+			if (sharedPrinters.Count > 0)
+			{
+				// Show the shared printers to the user
+				foreach (string printerName in sharedPrinters)
+				{
+					// Use Invoke to add items to the ListBox on the UI thread
+					if (lstOfPrinterName.InvokeRequired)
+					{
+						lstOfPrinterName.Invoke(new Action(() => lstOfPrinterName.Items.Add($"Server: {printerName}")));
+					}
+					else
+					{
+						lstOfPrinterName.Items.Add($"Server: {printerName}");
+					}
+				}
+
+				result = true;
+			}
+
+			return result;
+		}
+
+		private void UpdateProgressBar(int value)
+		{
+			// Ensure the value is within the valid range for the progress bar
+			value = Math.Max(0, Math.Min(100, value));
+
+			// Use Invoke to update the progress bar on the UI thread
+			if (prgBar.InvokeRequired)
+			{
+				prgBar.Invoke(new Action(() => prgBar.Value = value));
+			}
+			else
+			{
+				prgBar.Value = value;
 			}
 		}
 
